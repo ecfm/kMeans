3a78c5275340c21ac56d2a36ec6d8e07948d1218
cloudera <cloudera@quickstart.cloudera>
2016-02-03 16:27:04 -0800

    test running

--- src/edu/stanford/cs246/kMeans/kMeans.java
+++ src/edu/stanford/cs246/kMeans/kMeans.java
@@ -1,13 +1,21 @@
 package edu.stanford.cs246.kMeans;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import java.io.IOException;
 import java.util.Arrays;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -19,7 +27,9 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class kMeans extends Configured implements Tool {
-   private final int NUM_DIM = 58;
+   private final static int NUM_DIM = 58;
+   private final static int K = 10;
+   private static double sum_cost;
    private static double[][] centroids;
    public static void main(String[] args) throws Exception {
       System.out.println(Arrays.toString(args));
@@ -31,6 +41,22 @@ public class kMeans extends Configured implements Tool {
    @Override
    public int run(String[] args) throws Exception {
       System.out.println(Arrays.toString(args));
+      sum_cost = 0;
+      BufferedReader br = new BufferedReader(new FileReader(args[2]));
+  	  String line = null;
+  	  centroids = new double[K][NUM_DIM];
+  	  
+  	  int centroid_count = 0;
+  	  while ((line = br.readLine()) != null) {
+  		String[] val_str_array = line.split("\\s");
+        for (int i = 0; i < NUM_DIM; i++) {
+        	centroids[centroid_count][i] = Double.parseDouble(val_str_array[i]);
+        }
+        centroid_count += 1;
+      }
+  	  
+  	  
+  	  br.close();
       Job job = new Job(getConf(), "WordCount");
       job.setJarByClass(kMeans.class);
 
@@ -40,7 +66,7 @@ public class kMeans extends Configured implements Tool {
       job.setInputFormatClass(TextInputFormat.class);
       job.setOutputFormatClass(TextOutputFormat.class);
       
-      job.setOutputKeyClass(IntWritable.class);
+      job.setOutputKeyClass(NullWritable.class);
       job.setOutputValueClass(ArrayWritable.class);
  
       job.setMapOutputKeyClass(IntWritable.class);
@@ -50,7 +76,7 @@ public class kMeans extends Configured implements Tool {
       FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
       job.waitForCompletion(true);
-      
+      System.out.println(sum_cost);
       return 0;
    }
    
@@ -62,33 +88,62 @@ public class kMeans extends Configured implements Tool {
       public void map(LongWritable key, Text value, Context context)
               throws IOException, InterruptedException {
          String[] val_str_array = value.toString().split("\\s");
-         DoubleWritable[] val_array = new DoubleWritable[NUM_DIM]
+         DoubleWritable[] val_array = new DoubleWritable[NUM_DIM]; // store the cost at the end of the array 
          for (int i = 0; i < NUM_DIM; i++) {
-         	val_array[i].set(Double.parseDouble(val_str_array[i]));
+         	val_array[i] = new DoubleWritable(Double.parseDouble(val_str_array[i]));
          }
          double min_cost = Double.MAX_VALUE;
          int c = -1;
-         for (int i = 0; i < centroids.length; i++) {
-             double cost = euclideanDistance(centroids[i], val_array);
+         for (int i = 0; i < K; i++) {
+             double cost = euclideanDistanceCost(centroids[i], val_array);
              if (cost < min_cost) {
              	min_cost = cost;
              	c = i;
              }
          }
+         sum_cost += min_cost;
          ArrayWritable outputArray = new ArrayWritable(DoubleWritable.class);
-         context.write(c, outputArray);
+         outputArray.set(val_array);
+         context.write(new IntWritable(c), outputArray);
+      }
+      
+      private double euclideanDistanceCost(double[] centroid, DoubleWritable[] val_array){
+    	  double cost = 0;
+    	  for (int i = 0; i < NUM_DIM; i++) {
+    		  cost += Math.pow(centroid[i] - val_array[i].get(), 2);
+    	  }
+    	  return cost;
+      }
+      
+      private double manhattanDistanceCost(double[] centroid, DoubleWritable[] val_array){
+    	  double cost = 0;
+    	  for (int i = 0; i < NUM_DIM; i++) {
+    		  cost += Math.abs(centroid[i] - val_array[i].get());
+    	  }
+    	  return cost;
       }
    }
 
-   public static class Reduce extends Reducer<IntWritable, ArrayWritable, IntWritable, ArrayWritable> {
+   public static class Reduce extends Reducer<IntWritable, ArrayWritable, NullWritable, ArrayWritable> {
       @Override
       public void reduce(IntWritable key, Iterable<ArrayWritable> values, Context context)
               throws IOException, InterruptedException {
-         int sum = 0;
-         for (IntWritable val : values) {
-            sum += val.get();
+         double[] new_centroid = new double[NUM_DIM];
+         int num_points = 0;
+         for (ArrayWritable val : values) {
+        	 DoubleWritable[] val_array = (DoubleWritable[]) val.get();
+        	 for (int i = 0; i < NUM_DIM; i++) {
+        		 new_centroid[i] += val_array[i].get();
+        	 }
+        	 num_points += 1;
          }
-         context.write(key, new IntWritable(sum));
+         DoubleWritable[] new_centroid_cost = new DoubleWritable[NUM_DIM];
+         for (int i = 0; i < NUM_DIM; i++) {
+        	 new_centroid_cost[i] = new DoubleWritable(new_centroid[i]/num_points);
+         }
+         ArrayWritable outputArray = new ArrayWritable(DoubleWritable.class);
+         outputArray.set(new_centroid_cost);
+         context.write(NullWritable.get(), outputArray);
       }
    }
    
