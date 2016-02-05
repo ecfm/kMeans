0ad105d1f47a45b84d71d21b733f55ddad80a3f9
cloudera <cloudera@quickstart.cloudera>
2016-02-04 16:10:46 -0800

    done

--- src/edu/stanford/cs246/kMeans/kMeans.java
+++ src/edu/stanford/cs246/kMeans/kMeans.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 
 
 
+
+
 //import org.apache.hadoop.classification.InterfaceAudience;
 //import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
@@ -17,6 +19,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Counters;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -28,24 +32,32 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class kMeans extends Configured implements Tool {
-//   private final static int NUM_DIM = 58;
-//   private final static int K = 10;
-  private final static int NUM_DIM = 2;
-   private final static int K = 2;
-   private static double sum_cost;
-   private static double[][] centroids;
+   private final static int NUM_DIM = 58;
+   private final static int K = 10;
+   private final static int MAX_ITER = 20;
+   private final static double HUNDRED_K = 100000.0;
+   static enum COUNTER_NAME {COST};
    public static void main(String[] args) throws Exception {
       System.out.println(Arrays.toString(args));
       Configuration conf = new Configuration();
+      conf.set("distance", args[1]);
+      String centroid_title = args[2].split("\\.")[0];
+      String title_prefix = args[1]+"_"+centroid_title;
+      conf.set("title_prefix", title_prefix);
+      try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(conf.get("title_prefix")+"_total_cost.txt", false)))) {
+    	    out.print("");
+    	  }catch (IOException e) {
+    	    //exception handling left as an exercise for the reader
+    	  }
       int res = -1;
-      for (int i = 0; i < 6; i++){
+      for (int i = 0; i < MAX_ITER; i++){
     	  if (i == 0) {
     		  conf.set("centroid_file", args[2]);
     	  }
     	  else {
-    		  conf.set("centroid_file", "output"+Integer.toString(i - 1)+"/part-r-00000");
+    		  conf.set("centroid_file", title_prefix+"_output"+Integer.toString(i - 1)+"/part-r-00000");
     	  }
-    	  conf.set("output_dir", "output"+Integer.toString(i));
+    	  conf.set("output_dir", title_prefix+"_output"+Integer.toString(i));
     	  res = ToolRunner.run(conf, new kMeans(), args);
       }
       
@@ -57,25 +69,10 @@ public class kMeans extends Configured implements Tool {
 @Override
    public int run(String[] args) throws Exception {
       System.out.println(Arrays.toString(args));
-      sum_cost = 0;
       
       Configuration conf = getConf();
       
-      BufferedReader br = new BufferedReader(new FileReader(conf.get("centroid_file")));
-  	  String line = null;
-  	  centroids = new double[K][NUM_DIM];
   	  
-  	  int centroid_count = 0;
-  	  while ((line = br.readLine()) != null) {
-  		String[] val_str_array = line.split("\\s");
-        for (int i = 0; i < NUM_DIM; i++) {
-        	centroids[centroid_count][i] = Double.parseDouble(val_str_array[i]);
-        }
-        centroid_count += 1;
-      }
-  	  
-  	  
-  	  br.close();
       Job job = new Job(conf, "kMeans");
       job.setJarByClass(kMeans.class);
 
@@ -95,7 +92,10 @@ public class kMeans extends Configured implements Tool {
       FileOutputFormat.setOutputPath(job, new Path(conf.get("output_dir")));
 
       job.waitForCompletion(true);
-      try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("total_cost.txt", true)))) {
+      Counters counters = job.getCounters();
+      Counter cost_counter = counters.findCounter(COUNTER_NAME.COST);
+      double sum_cost = cost_counter.getValue()/HUNDRED_K;
+      try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(conf.get("title_prefix")+"_total_cost.txt", true)))) {
     	    out.println(sum_cost);
     	}catch (IOException e) {
     	    //exception handling left as an exercise for the reader
@@ -107,9 +107,8 @@ public class kMeans extends Configured implements Tool {
     private double[] values;
 
     public DoubleArrayWritable() {
-
-    }
-
+     }
+    
     public DoubleArrayWritable(double[] data) {
        this.values = data;
     }
@@ -150,21 +149,46 @@ public class kMeans extends Configured implements Tool {
       @Override
       public void map(LongWritable key, Text value, Context context)
               throws IOException, InterruptedException {
+    	  Configuration conf = context.getConfiguration();
+    	  BufferedReader br = new BufferedReader(new FileReader(conf.get("centroid_file")));
+     	  String line = null;
+     	 double[][] centroids = new double[K][NUM_DIM];
+     	  int centroid_count = 0;
+     	  while ((line = br.readLine()) != null) {
+     		String[] centroid_str_vector = line.split("\\s");
+           for (int i = 0; i < NUM_DIM; i++) {
+           	centroids[centroid_count][i] = Double.parseDouble(centroid_str_vector[i]);
+           }
+           centroid_count += 1;
+         }
+     	  br.close();
+     	  
          String[] val_str_array = value.toString().split("\\s");
          double[] val_array = new double[NUM_DIM]; // store the cost at the end of the array 
          for (int i = 0; i < NUM_DIM; i++) {
          	val_array[i] = Double.parseDouble(val_str_array[i]);
          }
+
+        
+         String dist_measure = conf.get("distance");
          double min_cost = Double.MAX_VALUE;
          int c = -1;
          for (int i = 0; i < K; i++) {
-             double cost = euclideanDistanceCost(centroids[i], val_array);
+             double cost = Double.MAX_VALUE;
+             if (dist_measure.equals("E")) {
+            	 cost = euclideanDistanceCost(centroids[i], val_array);
+             }
+             else if (dist_measure.equals("M")) {
+            	 cost = manhattanDistanceCost(centroids[i], val_array);
+             }
              if (cost < min_cost) {
              	min_cost = cost;
              	c = i;
              }
          }
-         sum_cost += min_cost;
+         Counter cost_counter = context.getCounter(COUNTER_NAME.COST);
+         long sum_cost = cost_counter.getValue()+(long)(min_cost*HUNDRED_K);
+         cost_counter.setValue(sum_cost);
          DoubleArrayWritable outputArray = new DoubleArrayWritable(val_array);
          context.write(new IntWritable(c), outputArray);
       }
@@ -206,11 +230,11 @@ public class kMeans extends Configured implements Tool {
             }
             new_centroid_str += Double.toString(new_centroid[i]/num_points);
          }
-         try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("centroids.txt", true)))) {
-     	    out.println(new_centroid_str);
-     	}catch (IOException e) {
-     	    //exception handling left as an exercise for the reader
-     	}
+//         try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("centroids_test.txt", true)))) {
+//     	    out.println(new_centroid_str);
+//     	}catch (IOException e) {
+//     	    //exception handling left as an exercise for the reader
+//     	}
          context.write(NullWritable.get(), new Text(new_centroid_str));
       }
    }
